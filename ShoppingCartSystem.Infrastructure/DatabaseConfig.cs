315d0e14f6a50b98671ca69cdaa9a676440d2486
Krishankant Duvey <krishankant.duvey@soti.net>
2020-03-29 11:05:54 +0530

    Db Part1

---
+++ ShoppingCartSystem.Infrastructure/DatabaseConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCartSystem.Abstraction.Model;
+using System;
+
+namespace ShoppingCartSystem.Infrastructure
+{
+    public class DatabaseConfig: DbContext
+    {
+        public DatabaseConfig() : base("ShoppinCartDB")
+        {
+            Database..SetInitializer<DatabaseConfig>(new ShoppingCartDbInitializer());
+        }
+        public DbSet<Cart> cart { get; set; }
+        public DbSet<LoginDetails> login { get; set; }
+        public DbSet<OrderDetail> orderDetail { get; set; }
+        public DbSet<Orders> orders { get; set; }
+        public DbSet<Products> products { get; set; }
+        public DbSet<Users> users { get; set; }
+    }
+}
---
+++ ShoppingCartSystem.Infrastructure/ShoppingCartDbInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCartSystem.Infrastructure
+{
+    public class ShoppingCartDbInitializer: DropCreateDatabaseAlways<DatabaseConfig>
+    {
+    }
+}
